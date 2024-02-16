3990f2b38feef087650e956ac2d693eceb4db132
Sarthak Joshi <sarthakjoshi.cs18@gmail.com>
2024-02-17 00:51:05 +0530

    Corrected charts link for mobile

--- src/components/base/MobileSideBar.tsx
+++ src/components/base/MobileSideBar.tsx
@@ -70,7 +70,7 @@ export default function MobileSideBar() {
                 </label>
                 <Link
                   className="flex transform items-center rounded-lg px-3 py-2 text-gray-200 transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700"
-                  href="dashboard/charts"
+                  href="/dashboard/charts"
                 >
                   <AreaChart className="h-5 w-5" aria-hidden="true" />
                   <span className="mx-2 text-sm font-medium">Charts</span>
