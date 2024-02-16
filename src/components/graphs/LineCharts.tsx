faa92b37354dd73f99b278cebceb741b6604d23b
Sarthak Joshi <sarthakjoshi.cs18@gmail.com>
2024-02-17 00:46:24 +0530

    removed div  in LineCharts.tsx

--- src/components/graphs/LineCharts.tsx
+++ src/components/graphs/LineCharts.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { DSAEncoding } from "crypto";
+
 import React from "react";
 import {
   LineChart,
@@ -18,16 +18,19 @@ export default function LineCharts({ data }: { data: DashboardData }) {
       height={430}
       className="border rounded-xl p-2 md:p-4  bg-white shadow-xl"
     >
-      <div className="flex flex-col justify-center items-center">
-        <LineChart width={600} height={300} data={data.visitors}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="count" stroke="#8884d8" />
-        </LineChart>
-      </div>
+      <LineChart
+        width={800}
+        height={400}
+        data={data.visitors}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="count" stroke="#8884d8" />
+      </LineChart>
     </ResponsiveContainer>
   );
 }
