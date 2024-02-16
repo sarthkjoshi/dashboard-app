fe702146045d4a522c9f896c99f220edc071597d
Sarthak Joshi <sarthakjoshi.cs18@gmail.com>
2024-02-16 23:04:58 +0530

    added responsive container for trendanalysis.tsx

--- src/components/graphs/TrendAnalysis.tsx
+++ src/components/graphs/TrendAnalysis.tsx
@@ -8,6 +8,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 
 const TrendAnalysis: React.FC<{ data: DashboardData }> = ({ data }) => {
@@ -47,25 +48,31 @@ const TrendAnalysis: React.FC<{ data: DashboardData }> = ({ data }) => {
           onChange={handleEndDateChange}
         />
       </div>
-      <LineChart
-        width={800}
-        height={400}
-        data={filteredData}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      <ResponsiveContainer
+        width="100%"
+        height={430}
+        className="border rounded-xl p-2 md:p-4  bg-white shadow-xl"
       >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="count"
-          stroke="#8884d8"
-          name="Visitors"
-        />
-        {/* Add other lines for additional metrics as needed */}
-      </LineChart>
+        <LineChart
+          width={800}
+          height={400}
+          data={filteredData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="count"
+            stroke="#8884d8"
+            name="Visitors"
+          />
+          {/* Add other lines for additional metrics as needed */}
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
